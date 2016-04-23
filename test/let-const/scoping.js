c9a60b0c717cd084dd2cdf12a11a52b1ed64c4c2
Dan Callahan <dan.callahan@gmail.com>
2016-04-23 00:12:52 -0500

    Add var / let / const exercises

---
+++ test/let-const/scoping.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+
+test.skip('`var` is function-scoped', t => {
+  // In JavaScript, `var` bindings are scoped at the function level.
+  //
+  // No matter where you declare a `var`, it applies everywhere in the function.
+
+  var x = 1;
+  {
+    var x = 2;   t.is(x, 2);
+  }
+
+  t.is(x, ___);
+});
+
+test.skip('`let` and `const` are block-scoped', t => {
+  // ES6 introduces `let` and `const` as block-scoped alternatives to `var`.
+  //
+  // Declarations are only valid within the nearest block.
+
+  let x = 1;
+  {
+    let x = 2;   t.is(x, 2);
+  }
+
+  t.is(x, ___);
+});
+
+// ============================================================================
+
+// REVIEW:
+// - You can use `const` or `let` instead of `var` to create bindings.
+// - `var` is function-scoped, `const` and `let` are block-scoped.
